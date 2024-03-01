65284bb0ea884b6fe04cc6e046e43c134da20048
yura <yur4ikkr@gmail.com>
2024-03-01 19:21:07 +0200

    first commit

---
+++ 1-biography/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type bornPlace string
+
+func (bp bornPlace) String() string {
+	return fmt.Sprintf("Was born in %s.", string(bp))
+}
+
+func main() {
+	name := "Albert Einstein"
+	birthDay := 14
+	birthMonth := "March"
+	birthYear := 1879
+	location := "Ulm, Kingdom of Württemberg, German Empire"
+	facts := []string{
+		"He was one of the greatest and most influential scientists of all time",
+		"Best known for developing the theory of relativity",
+		"He received the 1921 Nobel Prize in Physics",
+	}
+
+	bp := bornPlace(location)
+	biography := makeBiography(name, birthDay, birthMonth, birthYear, bp, facts)
+
+	fmt.Println(biography)
+}
+
+func makeBiography(name string, birthDay int, birthMonth string, birthYear int, location bornPlace, facts []string) string {
+	factsStr := ""
+	for _, fact := range facts {
+		factsStr += fmt.Sprintf("%s.\n", fact)
+	}
+
+	return fmt.Sprintf("%s (%d %s %d)\n%s\n%s", name, birthDay, birthMonth, birthYear, location, factsStr)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module prjctr
+
+go 1.22.0
